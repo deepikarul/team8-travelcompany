fde1ec4cec1ca17afded5ec7a18b43d6baec41e4
Deepika Arulselvan <deepikarul12@gmail.com>
2025-04-24 21:22:30 -0700

    edited About

--- api/backend_app.py
+++ api/backend_app.py
@@ -2,4 +2,4 @@ from backend.rest_entry import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug = True, host='0.0.0.0', port=4000)
--- app/src/pages/40_About.py
+++ app/src/pages/40_About.py
@@ -8,11 +8,12 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    Welcome to Explore Beyond! 
+    
+    We are a travel company that was created with the purpose of making it easier and more efficient to plan trips! 
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+    We hope these services make booking flights, hotels and activities stressfree!
 
-    Stay tuned for more information and features to come!
+    Our services also allow you to be able to work with a travel agent who will help plan the trip of your dreams!
     """
         )
